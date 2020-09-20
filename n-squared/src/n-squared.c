328aa324c5af31fae3a7123667ec026172b2dd87
Mohammad <MO@MacBook-Pro-13.local>
2020-09-20 11:58:18 -0700

    added new project

---
+++ n-squared/src/n-squared.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int num1 = 0;
+	printf("Enter a number: ");
+	scanf("%d", &num1);
+	printf("%d^2 = %d", num1, num1 * num1);
+
+	return 0;
+}
